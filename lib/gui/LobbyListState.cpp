60d18b2339da3fc7f0a0e88f384d6ffe075602fe
PhiVietHoang <phiviethoang14@gmail.com>
2023-07-27 11:01:15 +0700

    error join

--- lib/gui/LobbyListState.cpp
+++ lib/gui/LobbyListState.cpp
@@ -23,8 +23,51 @@ void LobbyListState::handleEvents(SDL_Event& e) {
             const SDL_Rect joinButtonRect = { 600, 100 + i * 50, 100, 30 };
             if (renderer.isPointInRect(mouseX, mouseY, joinButtonRect)) {
                 printf("Clicked on JOIN button!\n");
-                requestHelper->sendRequest(to_string(CommandMessage::JOIN) + " " + to_string(lobbyData[i]["id"]));
-                GameState::setCurrentState(GameState::State::IN_LOBBY);
+                
+                // Get the status of the room
+                bool isGameStarted = lobbyData[i]["isGameStarted"];
+                
+                if (!isGameStarted) {
+                    // If the room status is "Waiting," send the JOIN request to the server
+                    requestHelper->sendRequest(to_string(CommandMessage::JOIN) + " " + to_string(lobbyData[i]["id"]));
+                    GameState::setCurrentState(GameState::State::IN_LOBBY);
+                } else {
+                    // If the room status is "In Game," display a message that joining is not possible
+                    printf("Joining this room is not possible because it's already in a game.\n");
+                    // If the room status is "In Game," display a message that joining is not possible
+                    const SDL_MessageBoxButtonData buttons[] = {
+                        { SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 0, "OK" }
+                    };
+
+                    const SDL_MessageBoxColorScheme colorScheme = {
+                        { /* .colors (.r, .g, .b) */
+                            /* [SDL_MESSAGEBOX_COLOR_BACKGROUND] */
+                            { 255, 0, 0 },
+                            /* [SDL_MESSAGEBOX_COLOR_TEXT] */
+                            { 0, 255, 0 },
+                            /* [SDL_MESSAGEBOX_COLOR_BUTTON_BORDER] */
+                            { 255, 255, 0 },
+                            /* [SDL_MESSAGEBOX_COLOR_BUTTON_BACKGROUND] */
+                            { 0, 0, 255 },
+                            /* [SDL_MESSAGEBOX_COLOR_BUTTON_SELECTED] */
+                            { 255, 0, 255 }
+                        }
+                    };
+
+                    const SDL_MessageBoxData messageboxdata = {
+                        SDL_MESSAGEBOX_ERROR, /* .flags */
+                        NULL, /* .window */
+                        "Error", /* .title */
+                        "Joining this room is not possible because it's already in a game.", /* .message */
+                        SDL_arraysize(buttons), /* .numbuttons */
+                        buttons, /* .buttons */
+                        &colorScheme /* .colorScheme */
+                    };
+
+                    int buttonid;
+                    SDL_ShowMessageBox(&messageboxdata, &buttonid);
+                }
+
                 break; // No need to check further, as we found the clicked join button
             }
         }
